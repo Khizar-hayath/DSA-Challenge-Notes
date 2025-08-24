1f2464aeed06767f67974540e01b213c04e99643
Khizar <khizarhayath000@gmail.com>
2025-08-24 19:11:39 +0530

    Day 5

---
+++ Code/Arrays/004_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,59 @@
+//TITLE: REMOVE DUPLICATES IN PLACE FROM SORTED ARRAYS 
+//EXAMPLE: I/P: [1,1,1,2,3,4,4,4,5]   O/P: [1,2,3,4,5,1,1,4,4] starting all nos are sorted 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// BRUTE SOLUTION 
+//SOLN: Store the nos in set, set cannot have duplicate elements 
+//- TC: O(n log n)       - n(log(n)) to store in a set 
+//- SC: O(n) 
+
+int remove_duplicate_brute(vector<int> &arr, int n){             //Remember - Pass by arr reference only (&)
+  set<int> st;
+
+  for(int i = 0; i<n; i++){
+    st.insert(arr[i]);
+  }
+
+  int index = 0;
+  for(auto it: st){
+    arr[index] = it;
+    index++;
+  }
+  return index;
+}
+
+// OPTIMAL SOLUTION 
+//SOLN: Two pointer approach - i=arr[0] & j=arr[1], j scans the array. When j finds a new unique value, place it after i and move i index forward. This continues until all elements from staring become unique 
+//- TC: O(n) 
+//- SC: O(1) 
+int remove_duplicate_optimal(vector<int> &arr, int n){
+  if(n == 0) return 0;
+  
+  int i = 0; 
+  for(int j = 1; j<n; j++){
+    if(arr[i] != arr[j]){
+      arr[i+1] = arr[j];
+      i++;
+    }
+  }
+  return i+1;
+}
+
+
+int main(){
+  cout << "Enter number of elements in array: ";
+  int n;
+  cin >> n;
+  
+  cout << "Enter elements in array: ";
+  vector<int> arr(n);
+
+  for(int i = 0; i < n; i++) cin >> arr[i];
+  int ind = remove_duplicate_brute(arr, n);
+
+  for(int i = 0; i < ind; i++){
+    cout << arr[i] << "  ";
+  }
+}
---
+++ Code/Arrays/005_rotate_one_place.cpp
@@ -0,0 +1,36 @@
+//TITLE: ROATATE ARRAY BY 1 PLACE 
+//SOLN: Store first element in temp, move all elements 1 index befor and place temp at last index 
+//- TC: O(n) 
+//- SC: O(1) 
+//EXAMPLE: I/P: [1,2,3,4]  -> O/P [2,3,4,1]
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void left_rotate_1_place(vector<int>& arr, int n){
+  int temp = arr[0];
+  
+  for(int i = 1; i<n; i++){
+    arr[i - 1] = arr[i];
+  }
+  arr[n] = temp;
+}
+
+
+int main(){
+  cout << "Enter number of elements in array: ";
+  int n;
+  cin >> n;
+
+  cout << "Enter elements in array: ";
+  vector<int> arr(n);
+  for(int i = 0; i < n; i++) cin >> arr[i];
+
+  cout << "Left rotated array is: ";
+  left_rotate_1_place(arr, n);
+  for(int i = 0; i < n; i++){
+    cout << arr[i] << "  ";
+  }
+} 
