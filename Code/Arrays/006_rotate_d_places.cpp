6df6c35fb3b2fc35bc2739f9869f68434f35a921
Khizar <khizarhayath000@gmail.com>
2025-08-25 21:11:38 +0530

    Day 6

---
+++ Code/Arrays/006_rotate_d_places.cpp
@@ -0,0 +1,68 @@
+//TITLE: Left rotate the array by D places 
+//EXAMPLE: I/P: [1,2,3,4], d=1  -> O/P [2,3,4,1] 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//SOLN: Store d elements in temp array | Move elements by d places to right | Place values from temp array into original array 
+//- TC: O(n+d) 
+//- SC: O(d) 
+void left_rotate_dplaces_brute(vector<int>& arr, int n, int d){
+  vector<int> temp;
+
+  d = d % n;                          //NOTE: Because after 1 full rotation, the array remains same. Therefore finding remainder 
+
+  for(int i = 0; i < d; i++){
+    temp.push_back(arr[i]);
+  }
+
+  for(int i = d; i < n; i++){
+    arr[i - d] = arr[i];
+  }
+
+  for(int i = n-d; i<n; i++){
+    arr[i] = temp[i - (n-d)];
+  }
+}
+
+//SOLN: Rotate first d elements, then remaining n-d elements and finally rotate the whole array. Boom!! You found the answer 
+//- TC: O(2n) 
+//- SC: O(1) No extra space used 
+void left_rotate_dplaces_optimal(vector<int>& arr, int n, int d){
+  d = d % n;
+
+  reverse(arr.begin(), arr.begin() + d);
+  reverse(arr.begin()+d, arr.end());
+  reverse(arr.begin(), arr.end());
+}
+
+
+//! Learn to right rotate an array by d places too
+void right_rotate_dplaces(vector<int> &nums, int k){
+  int n = nums.size();
+  k = k%n;
+  reverse(nums.begin(), nums.begin()+(n-k));
+  reverse(nums.begin()+(n-k), nums.end());
+  reverse(nums.begin(), nums.end());
+}
+
+
+int main(){
+  cout << "Enter number of elements in array: ";
+  int n;
+  cin >> n;
+  cout << "Enter no of places array to rotate by: ";
+  int d;
+  cin >> d;
+
+  cout << "Enter elements in array: ";
+  vector<int> arr(n);
+  for(int i = 0; i < n; i++) cin >> arr[i];
+
+  cout << "Left rotated array is: ";
+  left_rotate_dplaces_optimal(arr, n, d);
+  for(int i = 0; i < n; i++){
+    cout << arr[i] << "  ";
+  }
+} 
---
+++ Code/Arrays/007_move_zeroes_to_end.cpp
@@ -0,0 +1,81 @@
+//TITLE: MOVE ZEROES TO END 
+// EXAMPLE: I/P: [0,1,0,3,12] -> O/P: [1,3,12,0,0]
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//SOLN: Store first element in temp | Move all elements 1 index before | Place temp at last index 
+//- TC: O(n) 
+//- SC: O(n) 
+void move_zeroes_to_end_brute(vector<int> &arr, int n)
+{
+  vector<int> temp;
+
+  // Place non-zero nos in temp array
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i] != 0)
+    {
+      temp.push_back(arr[i]);
+    };
+  }
+
+  // Fill the original array with non-zero elements
+  for (int i = 0; i < temp.size(); i++)
+  {
+    arr[i] = temp[i];
+  }
+
+  // Fill the remaining elements with zero
+  for (int i = temp.size(); i < n; i++)
+  {
+    arr[i] = 0;
+  }
+}
+
+
+//SOLN: Find the first element with 0 and initialise with j, if no 0 is found swap. Continue until end of array 
+//- TC: O(n) 
+//- SC: O(1) 
+void move_zeroes_to_end_optimal(vector<int> &arr, int n)
+{
+  int j = -1;
+
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i] == 0)
+    {
+      j = i;
+      break;
+    }
+  }
+
+  for (int i = j + 1; i < n; i++)
+  {                                                                             //Remember j = -1 satisfies perfectly if no zero is found
+    if (arr[i] != 0)
+    {
+      swap(arr[i], arr[j]);
+      j++;
+    }
+  }
+}
+
+int main()
+{
+  cout << "Enter number of elements in array: ";
+  int n;
+  cin >> n;
+
+  cout << "Enter elements in array: ";
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  cout << "Array with 0 at end is: ";
+  move_zeroes_to_end_optimal(arr, n);
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << "  ";
+  }
+}
