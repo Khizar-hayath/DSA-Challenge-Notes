98b07b6d402658f1e0c5812e9354d316749206eb
Khizar <khizarhayath000@gmail.com>
2025-08-30 20:31:38 +0530

    Day 1 edit(b)

--- Code/Arrays/001_largest_element.cpp
+++ Code/Arrays/001_largest_element.cpp
@@ -1,27 +1,32 @@
 //PROBLEM: Largest element in an array
-//SOLN: Traverse the array and check if any element is greater than the current largest element.
-//- TC ⏱️ for optimal: O(n) -> All cases 
-//- SC 🧾: O(1) -> All cases
+//EXAMPLE: I/P: [1,1,2,6,9,33,4]  -> O/P: 33 
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
+//BRUTE SOLUTION 
+//SOLN: Sort the elements and pick the greates one from the end 
+//- TC ⏱️: O(n*log(n)) 
+//- SC 🧾: O(log(n)) 
 int largestElement_brute(vector<int>& nums, int n) {
-  //NOTE: Sort the elements and pick the greates one from the end 
-  //- TC ⏱️ for brute: O(n*log(n)) -> All cases 
-  //- SC 🧾: O(log(n)) -> All cases
+  //Really basic, can be done
 }
 
-
+//OPTIMAL SOLUTION 
+//SOLN: Traverse the array and check if any element is greater than the current largest element.
+//- TC ⏱️: O(N) 
+//- SC 🧾: O(1) 
 int largestElement(vector<int>& nums, int n) {
   int largestnum = nums[0];
   for(int i = 1; i < n; i++){
-      if(nums[i] > largestnum){
-          largestnum = nums[i];
-      }
+    if(nums[i] > largestnum){
+        largestnum = nums[i];
+    }
   }
   return largestnum;
 }
+
 
 int main() {
   cout << "Enter number of elements in array: ";
@@ -31,5 +36,6 @@ int main() {
   cout << "Enter elements in array: ";
   vector<int> arr(n);
   for(int i = 0; i < n; i++) cin >> arr[i];
-  cout << largestElement(arr, n);
+
+  cout << "Largest element in the array is: " << largestElement(arr, n);
 }
